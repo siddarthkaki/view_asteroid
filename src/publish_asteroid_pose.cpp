1f74472bfebd4ff67090e0ee9af25b4f19244749
Marcelino <marcelino.malmeidan@gmail.com>
2018-07-27 15:18:30 -0500

    tumbling is now asteroid

--- src/publish_asteroid_pose.cpp
+++ src/publish_asteroid_pose.cpp
@@ -20,22 +20,47 @@ int main(int argc, char** argv){
 	ros::Publisher camera_pose = 
 		node.advertise<geometry_msgs::Pose>(cam_pose_topic, 1);
 
-	double omega = 0.25;
-	Eigen::Vector3d omega0(0.0, 0.0, 0.25);
-	ros::Time t0 = ros::Time::now();
+	// Initial conditions
+	// double omega = 0.25;
+	Eigen::Vector3d omega0(0.15, 0.1, 0.05);
+	Eigen::Vector3d quat_vec(0.0, -sin(M_PI/4), 0.0);
+	double quat_scalar = cos(M_PI/4);
+
+	// Initialize rk4 integrator
+	Eigen::Matrix3d J = Eigen::Vector3d(0.637, 2.122, 2.235).asDiagonal();
+	rk4 attitude_integrator(omega0, quat_scalar, quat_vec, J);
+
+	// States
+	Eigen::Vector3d omega_rk4;
+	Eigen::Quaterniond q_rk4;
+
+	// Initialize time
+	ros::Time t_prev = ros::Time::now();
+	ros::Time time_now;
+
+	// Update differential equations
 	while (ros::ok()) {
 
-		// Set asteroid's orientation
-		float dt = (ros::Time::now() - t0).toSec();
-		float roll = 0.0, pitch = -M_PI/2.0, yaw = omega*dt;
-		Eigen::Quaterniond q1, q2, q;
-		q1 = Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY());
-		q2 = Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ());
-		q = q2*q1;
-		geometry_msgs::Quaternion quat;
-		quat.x = q.x(); quat.y = q.y(); quat.z = q.z(); quat.w = q.w();
+		// // Set asteroid's orientation
+		// float dt = (ros::Time::now() - t0).toSec();
+		// float roll = 0.0, pitch = -M_PI/2.0, yaw = omega*dt;
+		// Eigen::Quaterniond q1, q2, q;
+		// q1 = Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY());
+		// q2 = Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ());
+		// q = q2*q1;
+		// geometry_msgs::Quaternion quat;
+		// quat.x = q.x(); quat.y = q.y(); quat.z = q.z(); quat.w = q.w();
 
-		// Asteroid pose
+		// Integrate RK4
+		time_now = ros::Time::now();
+		float dt = (time_now - t_prev).toSec();
+		attitude_integrator.UpdateStates(dt);
+		attitude_integrator.GetStates(&omega_rk4, &q_rk4);
+
+		// Set ROS structure for asteroid pose
+		geometry_msgs::Quaternion quat;
+		quat.x = q_rk4.x(); quat.y = q_rk4.y();
+		quat.z = q_rk4.z(); quat.w = q_rk4.w();
 		geometry_msgs::Point pos = helper::SetPoint(0.0, 0.0, 0.0);
 		geometry_msgs::Pose pose;
 		pose.position = pos;
@@ -50,11 +75,11 @@ int main(int argc, char** argv){
 		pose_cam.position = pos_cam;
 		pose_cam.orientation = quat_cam;
 
-
 		// Publish poses
 		asteroid_pose.publish(pose);
 		camera_pose.publish(pose_cam);
 
+		t_prev = time_now;
 		loop_rate.sleep();
 	}
 	
